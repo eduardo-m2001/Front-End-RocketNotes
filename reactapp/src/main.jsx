f4e1a9c7df8b6296a1e3d10efaef3fc274d2ce0a
eduardo <soudpqsoueu@hotmail.com>
2023-03-22 04:37:02 -0300

    add Profile and SignUp

--- reactapp/src/main.jsx
+++ reactapp/src/main.jsx
@@ -5,14 +5,14 @@ import GlobalStyles from './styles/global'
 
 import theme from './styles/theme'
 
-import { SignIn } from './pages/SignIn'
+import { Profile } from './pages/Profile'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <SignIn />
+      <Profile />
     </ThemeProvider>
   </React.StrictMode>,
 )
