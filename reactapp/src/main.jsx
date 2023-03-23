9285c53e4775a879a285545378dc76c4210ba022
eduardo <soudpqsoueu@hotmail.com>
2023-03-23 04:45:51 -0300

    add SignIn, SignUp and config routes

--- reactapp/src/main.jsx
+++ reactapp/src/main.jsx
@@ -5,14 +5,14 @@ import GlobalStyles from './styles/global'
 
 import theme from './styles/theme'
 
-import { Profile } from './pages/Profile'
+import { Routes } from './routes'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Profile />
+      <Routes />
     </ThemeProvider>
   </React.StrictMode>,
 )
