062bb9e3f3a41bfc432272e033ead67e1d224b5d
eduardo <soudpqsoueu@hotmail.com>
2023-04-20 18:16:09 -0300

    edit components

--- reactapp/src/components/ButtonText/index.jsx
+++
@@ -1,9 +0,0 @@
-import { Container } from './styles'
-
-export function ButtonText({ title, ...rest}) {
-  return (
-    <Container type="button" {...rest}>
-      {title}
-    </Container>
-  )
-}
--- reactapp/src/components/ButtonText/styles.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const Container = styled.button`
-  background: none;
-  color: ${({ theme }) => theme.COLORS.ORANGE};
-
-  border: none;
-  font-size: 1.6rem;
-`
